ce5c7957bb0d7060c752df013532e94c53735855
herruber <asteroth69@hotmail.com>
2018-09-19 21:13:58 +0200

    Initial fix arrows and other thingys, save definition as json and read it

--- PastePirate/Form1.Designer.cs
+++ PastePirate/Form1.Designer.cs
@@ -28,12 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.original = new System.Windows.Forms.TextBox();
+            this.definitions = new System.Windows.Forms.TextBox();
+            this.newCss = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // original
+            // 
+            this.original.Location = new System.Drawing.Point(12, 12);
+            this.original.Multiline = true;
+            this.original.Name = "original";
+            this.original.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.original.Size = new System.Drawing.Size(296, 495);
+            this.original.TabIndex = 0;
+            this.original.TextChanged += new System.EventHandler(this.original_TextChanged);
+            // 
+            // definitions
+            // 
+            this.definitions.Location = new System.Drawing.Point(644, 301);
+            this.definitions.Multiline = true;
+            this.definitions.Name = "definitions";
+            this.definitions.Size = new System.Drawing.Size(154, 206);
+            this.definitions.TabIndex = 1;
+            this.definitions.TextChanged += new System.EventHandler(this.definitions_TextChanged);
+            // 
+            // newCss
+            // 
+            this.newCss.Location = new System.Drawing.Point(314, 118);
+            this.newCss.Multiline = true;
+            this.newCss.Name = "newCss";
+            this.newCss.Size = new System.Drawing.Size(226, 389);
+            this.newCss.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 519);
+            this.Controls.Add(this.newCss);
+            this.Controls.Add(this.definitions);
+            this.Controls.Add(this.original);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox original;
+        private System.Windows.Forms.TextBox definitions;
+        private System.Windows.Forms.TextBox newCss;
     }
 }
 
--- PastePirate/Form1.cs
+++ PastePirate/Form1.cs
@@ -12,9 +12,351 @@ namespace PastePirate
 {
     public partial class Form1 : Form
     {
+
+        public List<ThemeColor> selectedColors = new List<ThemeColor>();
+
+        public List<ColorDefinition> colorDefs = new List<ColorDefinition>();
+        public List<Button> buttons = new List<Button>();
+
+
         public Form1()
         {
             InitializeComponent();
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 6; i++)
+            {
+
+                var btn = new Button();
+                btn.Name = "Color" + (i + 1);
+                btn.Text = btn.Name;
+
+                btn.Click += new System.EventHandler(this.button1_Click);
+                btn.Size = new Size(100, 25);
+                Controls.Add(btn);
+                btn.Location = new System.Drawing.Point(750, 10 + 30 * i);
+                btn.BackColor = Color.FromArgb(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+                buttons.Add(btn);
+
+                var d = new ColorDefinition();
+
+                d = SetFromColor(d, btn.BackColor, true);
+
+                colorDefs.Add(d);
+            }
+
         }
+
+        private bool ContainsColor(string line)
+        {
+
+            if (line.ToLower().Contains("color") || line.ToLower().Trim().StartsWith("c"))
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+        private string ReplaceColor(string line, ThemeColor o, List<ThemeColor> definitions)
+        {
+
+            for (int i = 0; i < definitions.Count; i++)
+            {
+
+                if (o.Equals(definitions[i]))
+                {
+                    line.Replace("", "");
+                }
+            }
+
+            return null;
+        }
+
+        private void original_TextChanged(object sender, EventArgs e)
+        {
+
+            string txt = original.Text;
+
+            string[] lines = txt.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            string newText = "";
+
+            foreach (var line in lines)
+            {
+                string t = line;
+                if (line.Length > 4)
+                {
+                    foreach (var d in colorDefs)
+                    {
+
+                        t = t.Replace(d.HexOld, d.HexNew);
+                        t = t.Replace(d.RgbOld, d.RgbNew);
+
+                    }
+
+
+                }
+                newCss.AppendText(t + "\n");
+
+            }
+
+
+
+        }
+
+        public ColorDefinition SetFromHex(ColorDefinition d, string hex, bool isNew)
+        {
+            if (isNew)
+            {
+                d.RgbNew = RGBConverter(hex);
+                d.HexNew = hex;
+            }
+            else
+            {
+                d.RgbOld = RGBConverter(hex);
+                d.HexOld = hex;
+            }
+
+            return d;
+        }
+
+        public ColorDefinition SetFromRgb(ColorDefinition d, string rgb, bool isNew)
+        {
+            if (isNew)
+            {
+                d.RgbNew = rgb;
+                d.HexNew = HexConverter(rgb);
+            }
+            else
+            {
+                d.RgbOld = rgb;
+                d.HexOld = HexConverter(rgb);
+            }
+
+            return d;
+        }
+
+        public ColorDefinition SetFromColor(ColorDefinition d, Color c, bool isNew)
+        {
+            if (isNew)
+            {
+                d.RgbNew = c.R + "," + c.G + "," + c.B;
+                d.HexNew = HexConverter(c);
+            }
+            else
+            {
+                d.RgbOld = c.R + "," + c.G + "," + c.B;
+                d.HexOld = HexConverter(c);
+            }
+
+            return d;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (sender.GetType() == typeof(Button))
+            {
+
+                using (ColorDialog dlg= new ColorDialog())
+                {
+
+                    if (dlg.ShowDialog() == DialogResult.OK)
+                    {
+
+                        Button btn = (sender as Button);
+                        btn.BackColor = dlg.Color;
+
+                        colorDefs[buttons.IndexOf(btn)] = SetFromColor(colorDefs[buttons.IndexOf(btn)], btn.BackColor, true);
+
+
+                    }
+                }
+
+            }
+        }
+
+        private static string HexConverter(Color c)
+        {
+            return "#" + c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2");
+        }
+
+        private static string RGBConverter(string hex)
+        {
+            hex = hex.Replace("#", "");
+
+            string c1 = hex[0] +"" +hex[1];
+            string c2 = hex[2] + "" + hex[3];
+            string c3 = hex[4] + "" + hex[5];
+
+
+            int R = int.Parse(c1, System.Globalization.NumberStyles.HexNumber);
+            int G = int.Parse(c2, System.Globalization.NumberStyles.HexNumber);
+            int B = int.Parse(c3, System.Globalization.NumberStyles.HexNumber);
+
+            return R + "," + G + "," + B;
+        }
+
+        private static string HexConverter(string rgb)
+        {
+
+            string[] info = rgb.Split(',');
+
+            int r = int.Parse(info[0]);
+            int g = int.Parse(info[1]);
+            int b = int.Parse(info[2]);
+
+
+            return "#" + r.ToString("X2") + g.ToString("X2") + b.ToString("X2");
+        }
+
+        private void definitions_TextChanged(object sender, EventArgs e)
+        {
+
+            ColorConverter converter = new ColorConverter();
+            string[] lines = definitions.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> colors = new List<string>();
+
+            foreach (var line in lines)
+            {
+
+                string bajs = line.Replace("\t", " ").Replace("\r", " ");
+
+                string[] parts = bajs.Trim().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var part in parts)
+                {
+                    if (part.Contains("#"))
+                    {
+                        colors.Add(part);
+                    }
+                }
+
+            }
+
+            for (int i = 0; i < colors.Count; i++)
+            {
+
+                colorDefs[i] = SetFromHex(colorDefs[i], colors[i], false);
+
+
+            }
+
+
+
+        }
+
+
+    }
+
+    public class ColorDefinition
+    {
+
+        public string HexOld { get; set; }
+        public string RgbOld { get; set; }
+
+        public string HexNew { get; set; }
+        public string RgbNew { get; set; }
+
+        public ColorDefinition(string[] hex, string[] rgb)
+        {
+            HexOld = hex[0];
+            HexNew = hex[1];
+
+            RgbOld = rgb[0];
+            RgbNew = rgb[1];
+        }
+
+        public ColorDefinition()
+        {
+
+        }
+
+    }
+
+    public class ThemeColor
+    {
+
+        public int R { get; set; }
+        public int G { get; set; }
+        public int B { get; set; }
+        public float A { get; set; }
+
+        public string Hex { get; set; }
+        public bool isDefinition = false;
+
+        public ThemeColor(string rgbOrHex)
+        {
+            string trimmed = rgbOrHex.Trim();
+
+            string[] colors;
+
+            //If a color definition
+            if (trimmed.StartsWith("c"))
+            {
+                isDefinition = true;
+                colors = trimmed.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var color in colors)
+                {
+                    string t = color.Trim();
+                    //Is a hex defintion
+                    if (t.StartsWith("#"))
+                    {
+                        Hex = t;
+                    }
+                    else if(t.Contains(","))
+                    {
+
+                        string[] rgb = t.Split(',');
+
+                        R = int.Parse(rgb[0]);
+                        G = int.Parse(rgb[1]);
+                        B = int.Parse(rgb[2]);
+
+                        if (rgb.Length == 4)
+                        {
+                            A = float.Parse(rgb[3]);
+                        }
+                        else
+                        {
+                            A = 1f;
+                        }
+
+                    }
+                }
+
+            }
+            else
+            {
+
+            }
+
+        }
+
+        public bool Equals(ThemeColor c)
+        {
+
+            if (R == c.R && G == c.G && B == c.B)
+            {
+                return true;
+            }
+            else if (Hex.ToLower().Equals(c.Hex.ToLower()))
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public ThemeColor(string rgb, string hex)
+        {
+
+        }
+
     }
 }
